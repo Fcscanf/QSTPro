5d72e2c4bf6f9ddd3a51e5ce40e5fa3bc0e03946
Fcscanf <fcscanf@gmail.com>
2019-08-25 10:51:44 +0800

    [Codes-Fcant-Updates-Pro(都市生活)]Updates:Web&Server-完成用户手册的下载功能实现,实现是通过获取WEB-INF下的class类路径+文件名(为了防止多文件在类路径下新建文件夹file,所以在获取时多加了file路径)然后通过ResponseEntity<byte[]>实现返回输出

---
+++ DL/src/main/java/com/fcant/dl/controller/FileDownloadController.java
@@ -0,0 +1,48 @@
+package com.fcant.dl.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+
+/**
+ * FileDownloadController
+ * <p>
+ * encoding:UTF-8
+ *
+ * @author Fcant
+ * @date 上午 10:02 2019-08-25/0025
+ */
+@Controller
+@RequestMapping("/file")
+public class FileDownloadController {
+
+    /**
+     * 文件下载
+     *
+     * @param filename 文件名
+     * @return File
+     * @author Fcant
+     * @date 上午 10:04 2019-08-25/0025
+     */
+    @RequestMapping("/download")
+    ResponseEntity<byte[]> getUserBook(String filename) throws URISyntaxException, IOException {
+        File file = new File(this.getClass().getClassLoader().getResource("").toURI().getPath()+"\\file\\"+filename);
+        byte[] body = null;
+        InputStream inputStream = new FileInputStream(file);
+        body = new byte[inputStream.available()];
+        inputStream.read(body);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "attchement;filename=" + file.getName());
+        HttpStatus statusCode = HttpStatus.OK;
+        ResponseEntity<byte[]> entity = new ResponseEntity<byte[]>(body, headers, statusCode);
+        return entity;
+    }
+}
